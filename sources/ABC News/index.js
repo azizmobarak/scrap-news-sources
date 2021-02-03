fdcf82b31d4dff6cb8379367645624ee38636628
aziz mobarak <mobarakaziz9@gmail.com>
2021-02-03 11:33:28 +0100

    fixed

--- sources/ABC News/index.js
+++ sources/ABC News/index.js
@@ -2,7 +2,10 @@ const puppeteer  = require('puppeteer-extra');
 const puppeteer_stealth = require('puppeteer-extra-plugin-stealth');
 const puppeteer_agent = require('puppeteer-extra-plugin-anonymize-ua');
 const Recaptcha = require('puppeteer-extra-plugin-recaptcha');
+const AdblockerPlugin = require('puppeteer-extra-plugin-adblocker')
 
+//block ads
+puppeteer.use(AdblockerPlugin())
 // stealth
 puppeteer.use(puppeteer_stealth())
 // captcha configuration
@@ -13,9 +16,9 @@ puppeteer.use(
     })
 );
 
-puppeteer.use(puppeteer_agent());
+//puppeteer.use(puppeteer_agent());
 
-var Categories=['Politics','Entertainment','Technology','Health','Sports','International','Business','Lifestyle'];
+var Categories=['Politics','Entertainment','Technology','Health','Sports','International','Business'];
 
 const ABC_NEWS = () =>{
     (async()=>{
@@ -34,7 +37,7 @@ const ABC_NEWS = () =>{
 
        var page = await browser.newPage();
        var AllData=[];  
-       var PageData=[];
+
      // boucle on categories started 
      for(let i=0;i<Categories.length;i++){
 
@@ -44,152 +47,86 @@ const ABC_NEWS = () =>{
 
         //navigate to category sub route
         await page.goto(['https://abcnews.go.com/','',Category].join(''));
-        await page.waitForNavigation({ waitUntil: 'load' }) //networkidle0
+      //  await page.waitForNavigation({ waitUntil: 'load' }) //networkidle0
 
-         switch(Category){
-             case "Politics" :
+        /*try{
+            await page.waitForSelector(".animation-container");
+        }catch(e){
+        console.log(e)
+        }*/
 
-                   // get the data from the page
-               PageData = await page.evaluate((Category)=>{
-                       var titles = document.querySelectorAll('.ContentRoll__Headline>h2>a.AnchorLink');
-                       var images =document.querySelectorAll('.Image__Wrapper>picture>img');
-                       var time = document.querySelectorAll('.ContentRoll__TimeStamp .TimeStamp__Date ContentRoll__Date--recent')
-                       var data =[];
-                       
-                       for(let j=0;j<titles.length;j++){
-                        if(WordExist(time[j])==true){
-                            data.push({
-                                title : titles[j].textContent,
-                                link : titles[j].href,
-                                images :  typeof(images[j])=="undefined" ? null : images[j].src,
-                                Category:Category,
-                                source :"ABC NEWS",
-                                sourceLink:"https://abcnews.go.com",
-                                sourceLogo:"https://gray-wbay-prod.cdn.arcpublishing.com/resizer/fln06LgHS8awdDtCHhWoikKI7UE=/1200x675/smart/cloudfront-us-east-1.images.arcpublishing.com/gray/X3TAX5IMPBHY7EBGM6XW47YETE.jpg"
-                               });
+        
+         // get the data from the page
+         var PageData = await page.evaluate((Category)=>{
+
+               
+               // function to look for a word inside other words
+                   const WordExist=(searchIn)=>{
+                    if(searchIn.indexOf("second")!=-1){
+                         return true;
+                         }else{
+                       if(searchIn.indexOf("seconds")!=-1){
+                      return true;
+                       }else{
+                         if(searchIn.indexOf("minute")!=-1){
+                       return true;
+                       }else{
+                       if(searchIn.indexOf("minutes")!=-1){
+                           return true;
+                          }else{
+                        if(searchIn.indexOf("hour")!=-1){
+                          return true;
+                         }else{
+                         if(searchIn.indexOf("hours")!=-1){
+                            return true;
+                        }else{
+                            return false;
                         }
-                      }
-                      return data;
-                 },Category);
-                
-                 PageData.map(item=>{
-                     AllData.push(item);
-                 });
-
-                     break;
-
-
-                case "Entertainment":
-
-                      // get the data from the page
-                        PageData = await page.evaluate((Category)=>{
-                                 var titles = document.querySelectorAll('.ContentRoll__Headline>h2>a.AnchorLink');
-                                 var images =document.querySelectorAll('.ContentRoll__Image img');
-                                 var time = document.querySelectorAll('.ContentRoll__TimeStamp .TimeStamp__Date ContentRoll__Date--recent')
-                                 
-                                var data =[];
-                                for(let j=0;j<titles.length;j++){
-                                
-                                    if(WordExist(time[j])==true){
-                                        data.push({
-                                            title : titles[j].textContent,
-                                            link : titles[j].href,
-                                            images :  typeof(images[j])=="undefined" ? null : images[j].src,
-                                            Category:Category,
-                                            source :"ABC NEWS",
-                                            sourceLink:"https://abcnews.go.com",
-                                            sourceLogo:"https://gray-wbay-prod.cdn.arcpublishing.com/resizer/fln06LgHS8awdDtCHhWoikKI7UE=/1200x675/smart/cloudfront-us-east-1.images.arcpublishing.com/gray/X3TAX5IMPBHY7EBGM6XW47YETE.jpg"
-                                           });
-                                    }
-
-                                    }
-                       return data;
-                      },Category);
- 
-                     PageData.map(item=>{
-                                AllData.push(item);
-                               });
-                   break;
-
-
-
-            case "Technology":
-
-           
-                      // get the data from the page
-                      PageData = await page.evaluate((Category)=>{
-                        var titles = document.querySelectorAll('.ContentRoll__Headline>h2>a.AnchorLink');
-                        var images =document.querySelectorAll('.ContentRoll__Image img');
-                        var time = document.querySelectorAll('.ContentRoll__TimeStamp .TimeStamp__Date ContentRoll__Date--recent')
-                        
-                       var data =[];
-                       for(let j=0;j<titles.length;j++){
-                       
-                           if(WordExist(time[j])==true){
-                               data.push({
-                                   title : titles[j].textContent,
-                                   link : titles[j].href,
-                                   images :  typeof(images[j])=="undefined" ? null : images[j].src,
-                                   Category:Category,
-                                   source :"ABC NEWS",
-                                   sourceLink:"https://abcnews.go.com",
-                                   sourceLogo:"https://gray-wbay-prod.cdn.arcpublishing.com/resizer/fln06LgHS8awdDtCHhWoikKI7UE=/1200x675/smart/cloudfront-us-east-1.images.arcpublishing.com/gray/X3TAX5IMPBHY7EBGM6XW47YETE.jpg"
-                                  });
-                           }
-
-                           }
-              return data;
-             },Category);
-
-            PageData.map(item=>{
-                       AllData.push(item);
-                      });
-
-
-                   break;
-
-
-               default :
-                   console.log('default');
-                   break;
-         }        
-       }
-       console.log(AllData);
-
-    })();
-}
-
-
-
-
-// function to look for a word inside other words
-const WordExist=(searchIn)=>{
-if(searchIn.indexOf("second")!=-1){
-    return true;
-}else{
-    if(searchIn.indexOf("seconds")!=-1){
-        return true;
-    }else{
-        if(searchIn.indexOf("minute")!=-1){
-            return true;
-        }else{
-            if(searchIn.indexOf("minutes")!=-1){
-                return true;
-            }else{
-                if(searchIn.indexOf("hour")!=-1){
-                    return true;
-                }else{
-                    if(searchIn.indexOf("hours")!=-1){
-                        return true;
-                    }else{
-                        return false;
                     }
                 }
             }
         }
     }
+    }
+    
+
+                     var titles = document.querySelectorAll('.ContentRoll__Headline>h2>a.AnchorLink');
+                     var images =document.querySelectorAll('.ContentRoll__Image img');
+                     var time = document.querySelectorAll('.ContentRoll__Date')
+            
+                     
+                var data =[];
+         for(let j=0;j<titles.length;j++){
+           
+              if(WordExist(time[j].textContent)==true){
+                   data.push({
+                       time : Date.now(),
+                       title : titles[j].textContent,
+                       link : titles[j].href,
+                       images :  typeof(images[j])=="undefined" ? null : images[j].src,
+                       Category:Category,
+                       source :"ABC NEWS",
+                       sourceLink:"https://abcnews.go.com",
+                       sourceLogo:"https://gray-wbay-prod.cdn.arcpublishing.com/resizer/fln06LgHS8awdDtCHhWoikKI7UE=/1200x675/smart/cloudfront-us-east-1.images.arcpublishing.com/gray/X3TAX5IMPBHY7EBGM6XW47YETE.jpg"
+                      });
+                   }
+               }
+                      return data;
+               },Category);
+
+               PageData.map(item=>{
+                   AllData.push(item)
+               })
+       }
+
+     console.log(AllData);
+
+     await browser.close();
+    })();
 }
-}
+
+
+
 
 
 module.exports=ABC_NEWS;
