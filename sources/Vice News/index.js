ef9ee051f9b92999cd9c9d9f311b3c29d48352ea
aziz mobarak <mobarakaziz9@gmail.com>
2021-02-12 06:51:23 +0100

    vice

--- sources/Vice News/index.js
+++ sources/Vice News/index.js
@@ -188,7 +188,7 @@ const GetContent = async(page,data)=>{
      }
     
 
-    if(Content!=null && Content!=""){
+    if(((Content!=null && Content!="") && item.type==="article") || item.type==="video"){
           AllData_WithConetent.push({
                 time : Date.now(),
                 title : item.title,
