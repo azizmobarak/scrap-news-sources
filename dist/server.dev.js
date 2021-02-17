50988799b4b970b215604eea48262d9a835243e0
aziz mobarak <mobarakaziz9@gmail.com>
2021-02-17 14:44:07 +0100

    huffpost

--- dist/server.dev.js
+++ dist/server.dev.js
@@ -28,135 +28,101 @@ con.connect(process.env.DATABASE, {
     // else{
     //     console.log(doc)
     // }
-    // })
+    // });
   }
-});
-cron.schedule('* 1 * * *', function () {
-  console.log(1); // 1 hour
+}); // //cron.schedule('* 1 * * *', () => {
+//     // 1 hour
+//     const ABC_NEWS = require('./sources/ABC News');
+//      ABC_NEWS();
+//   //});
+// //cron.schedule('45 * * * *', () => {
+//     // 45 hour
+//     const BBC = require('./sources/BBC');
+//      BBC();
+//   //});
+// cron.schedule('15 * * * *', () => {
+//  // 15 min
+// const Bloomberg = require('./sources/Bloomberg');
+// Bloomberg();
+//   });
+// cron.schedule('20 * * * *', () => {
+//   // 15 min
+//   const CBC = require('./sources/CBS News');
+//   CBC();
+//      });
+// cron.schedule('23 * * * *', () => {
+//   //15 min
+//   const CNET = require('./sources/CNET');
+//   CNET();
+// });
+// cron.schedule('30 * * * *', () => {
+//     //30
+//  const CNN = require('./sources/CNN');
+//  CNN();
+//   });
+// cron.schedule('20 * * * *', () => {
+//   // 20
+//    const FOXNEWS = require('./sources/Fox News Channel');
+//    FOXNEWS();
+//   });
+// cron.schedule('* 22 * * *', () => {
+// // with low content and link to page contain videos about the movie/serie run it for 24h and more.
+// const HBO = require('./sources/HBO');
+// HBO();
+// });
 
-  var ABC_NEWS = require('./sources/ABC News');
-
-  ABC_NEWS();
-});
-cron.schedule('45 * * * *', function () {
-  console.log(2); // 45 hour
-
-  var BBC = require('./sources/BBC');
-
-  BBC();
-});
-cron.schedule('15 * * * *', function () {
-  console.log(3); // 15 min
-
-  var Bloomberg = require('./sources/Bloomberg');
-
-  Bloomberg();
-});
-cron.schedule('20 * * * *', function () {
-  console.log(4); // 15 min
-
-  var CBC = require('./sources/CBS News');
-
-  CBC();
-});
-cron.schedule('23 * * * *', function () {
-  console.log(5); //15 min
-
-  var CNET = require('./sources/CNET');
-
-  CNET();
-});
-cron.schedule('30 * * * *', function () {
-  console.log(6); //30
-
-  var CNN = require('./sources/CNN');
-
-  CNN();
-});
-cron.schedule('20 * * * *', function () {
-  console.log(7); // 20
-
-  var FOXNEWS = require('./sources/Fox News Channel');
-
-  FOXNEWS();
-});
-cron.schedule('* 22 * * *', function () {
-  console.log(8); // with low content and link to page contain videos about the movie/serie run it for 24h and more.
-
-  var HBO = require('./sources/HBO');
-
-  HBO();
-});
 cron.schedule('28 * * * *', function () {
-  console.log(9); // 30 min
-
+  // 30 min
   var HuffPost = require('./sources/HuffPost');
 
   HuffPost();
-});
-cron.schedule('10 1 * * *', function () {
-  console.log(10); //1h
+}); // cron.schedule('10 1 * * *', () => {
+// //1h
+//  const LosAngelesNews = require('./sources/Los Angeles Daily News');
+//  LosAngelesNews();
+// });
+// cron.schedule('5 1 * * *', () => {
+//     //1h
+//      const LosAngelesNews = require('./sources/Los Angeles Daily News');
+//      LosAngelesNews();
+// });
+// cron.schedule('3 1 * * *', () => {
+//   //1h
+// const LosAngeleceTimes = require('./sources/Los Angeles Times');
+// LosAngeleceTimes();
+// });
+// cron.schedule('8 1 * * *', () => {
+//     //1h
+// const MSNBC = require('./sources/MSNBC');
+// MSNBC()
+// });
+// cron.schedule('35 * * * *', () => {
+//     console.log(14)
+//     //1h
+// const Gardian = require('./sources/The Guardian');
+// Gardian();
+// });
+// cron.schedule('33 * * * *', () => {
+//     //1h
+// const NEWYORKTIMES = require("./sources/The New York Times");
+// NEWYORKTIMES();
+// });
+// cron.schedule('40 * * * *', () => {
+//     //1h
+// const WALLSTREET = require('./sources/The Wall Street Journal');
+// WALLSTREET();
+// });
+// cron.schedule('55 * * * *', () => {
+//     //1h
+// const WASHINGTONPOST = require('./sources/The Washington Post');
+// WASHINGTONPOST();
+// });
+// cron.schedule('50 * * * *', () => {
+//     //1h
+// const VICENEWS = require('./sources/Vice News');
+// VICENEWS();
+// });
 
-  var LosAngelesNews = require('./sources/Los Angeles Daily News');
-
-  LosAngelesNews();
-});
-cron.schedule('5 1 * * *', function () {
-  console.log(11); //1h
-
-  var LosAngelesNews = require('./sources/Los Angeles Daily News');
-
-  LosAngelesNews();
-});
-cron.schedule('3 1 * * *', function () {
-  console.log(12); //1h
-
-  var LosAngeleceTimes = require('./sources/Los Angeles Times');
-
-  LosAngeleceTimes();
-});
-cron.schedule('8 1 * * *', function () {
-  console.log(13); //1h
-
-  var MSNBC = require('./sources/MSNBC');
-
-  MSNBC();
-});
-cron.schedule('35 * * * *', function () {
-  console.log(14); //1h
-
-  var Gardian = require('./sources/The Guardian');
-
-  Gardian();
-});
-cron.schedule('33 * * * *', function () {
-  console.log(15); //1h
-
-  var NEWYORKTIMES = require("./sources/The New York Times");
-
-  NEWYORKTIMES();
-});
-cron.schedule('40 * * * *', function () {
-  console.log(16); //1h
-
-  var WALLSTREET = require('./sources/The Wall Street Journal');
-
-  WALLSTREET();
-});
-cron.schedule('55 * * * *', function () {
-  console.log(17); //1h
-
-  var WASHINGTONPOST = require('./sources/The Washington Post');
-
-  WASHINGTONPOST();
-});
-cron.schedule('50 * * * *', function () {
-  console.log(18); //1h
-
-  var VICENEWS = require('./sources/Vice News');
-
-  VICENEWS();
-});
 app.listen(PORT, function () {
   console.log('connected at ' + PORT);
 });
--- server.js
+++ server.js
@@ -17,148 +17,131 @@ con.connect(process.env.DATABASE,{useNewUrlParser: true,useUnifiedTopology: true
         // else{
         //     console.log(doc)
         // }
-        // })
+        // });
     }
 });
 
 
 
-cron.schedule('* 1 * * *', () => {
-    console.log(1)
-    // 1 hour
-    const ABC_NEWS = require('./sources/ABC News');
-    ABC_NEWS();
-  });
+// //cron.schedule('* 1 * * *', () => {
+//     // 1 hour
+//     const ABC_NEWS = require('./sources/ABC News');
+//      ABC_NEWS();
+//   //});
 
 
 
-cron.schedule('45 * * * *', () => {
-    console.log(2)
-    // 45 hour
-   const BBC = require('./sources/BBC');
-   BBC();
-  });
+// //cron.schedule('45 * * * *', () => {
+//     // 45 hour
+//     const BBC = require('./sources/BBC');
+//      BBC();
+//   //});
 
 
-cron.schedule('15 * * * *', () => {
-    console.log(3)
- // 15 min
-const Bloomberg = require('./sources/Bloomberg');
-Bloomberg();
-  });
+// cron.schedule('15 * * * *', () => {
+//  // 15 min
+// const Bloomberg = require('./sources/Bloomberg');
+// Bloomberg();
+//   });
 
 
-cron.schedule('20 * * * *', () => {
-    console.log(4)
-  // 15 min
-  const CBC = require('./sources/CBS News');
-  CBC();
-     });
+// cron.schedule('20 * * * *', () => {
+//   // 15 min
+//   const CBC = require('./sources/CBS News');
+//   CBC();
+//      });
 
 
-cron.schedule('23 * * * *', () => {
-    console.log(5)
-  //15 min
-  const CNET = require('./sources/CNET');
-  CNET();
-});
+// cron.schedule('23 * * * *', () => {
+//   //15 min
+//   const CNET = require('./sources/CNET');
+//   CNET();
+// });
 
 
-cron.schedule('30 * * * *', () => {
-    console.log(6)
-    //30
- const CNN = require('./sources/CNN');
- CNN();
-  });
+// cron.schedule('30 * * * *', () => {
+//     //30
+//  const CNN = require('./sources/CNN');
+//  CNN();
+//   });
 
 
-cron.schedule('20 * * * *', () => {
-    console.log(7)
-  // 20
-   const FOXNEWS = require('./sources/Fox News Channel');
-   FOXNEWS();
-  });
+// cron.schedule('20 * * * *', () => {
+//   // 20
+//    const FOXNEWS = require('./sources/Fox News Channel');
+//    FOXNEWS();
+//   });
 
 
-cron.schedule('* 22 * * *', () => {
-    console.log(8)
-// with low content and link to page contain videos about the movie/serie run it for 24h and more.
-const HBO = require('./sources/HBO');
-HBO();
-});
+// cron.schedule('* 22 * * *', () => {
+// // with low content and link to page contain videos about the movie/serie run it for 24h and more.
+// const HBO = require('./sources/HBO');
+// HBO();
+// });
 
 
 cron.schedule('28 * * * *', () => {
-    console.log(9)
 // 30 min
  const HuffPost = require('./sources/HuffPost');
  HuffPost();
 });
 
-cron.schedule('10 1 * * *', () => {
-    console.log(10)
-//1h
- const LosAngelesNews = require('./sources/Los Angeles Daily News');
- LosAngelesNews();
-});
+// cron.schedule('10 1 * * *', () => {
+// //1h
+//  const LosAngelesNews = require('./sources/Los Angeles Daily News');
+//  LosAngelesNews();
+// });
 
 
-cron.schedule('5 1 * * *', () => {
-    console.log(11)
-    //1h
-     const LosAngelesNews = require('./sources/Los Angeles Daily News');
-     LosAngelesNews();
-});
+// cron.schedule('5 1 * * *', () => {
+//     //1h
+//      const LosAngelesNews = require('./sources/Los Angeles Daily News');
+//      LosAngelesNews();
+// });
 
-cron.schedule('3 1 * * *', () => {
-    console.log(12)
-  //1h
-const LosAngeleceTimes = require('./sources/Los Angeles Times');
-LosAngeleceTimes();
-});
+// cron.schedule('3 1 * * *', () => {
+//   //1h
+// const LosAngeleceTimes = require('./sources/Los Angeles Times');
+// LosAngeleceTimes();
+// });
 
-cron.schedule('8 1 * * *', () => {
-    console.log(13)
-    //1h
-const MSNBC = require('./sources/MSNBC');
-MSNBC()
-});
+// cron.schedule('8 1 * * *', () => {
+//     //1h
+// const MSNBC = require('./sources/MSNBC');
+// MSNBC()
+// });
 
-cron.schedule('35 * * * *', () => {
-    console.log(14)
-    //1h
-const Gardian = require('./sources/The Guardian');
-Gardian();
-});
+// cron.schedule('35 * * * *', () => {
+//     console.log(14)
+//     //1h
+// const Gardian = require('./sources/The Guardian');
+// Gardian();
+// });
 
-cron.schedule('33 * * * *', () => {
-    console.log(15)
-    //1h
-const NEWYORKTIMES = require("./sources/The New York Times");
-NEWYORKTIMES();
-});
+// cron.schedule('33 * * * *', () => {
+//     //1h
+// const NEWYORKTIMES = require("./sources/The New York Times");
+// NEWYORKTIMES();
+// });
 
-cron.schedule('40 * * * *', () => {
-    console.log(16)
-    //1h
-const WALLSTREET = require('./sources/The Wall Street Journal');
-WALLSTREET();
-});
+// cron.schedule('40 * * * *', () => {
+//     //1h
+// const WALLSTREET = require('./sources/The Wall Street Journal');
+// WALLSTREET();
+// });
 
 
-cron.schedule('55 * * * *', () => {
-    console.log(17)
-    //1h
-const WASHINGTONPOST = require('./sources/The Washington Post');
-WASHINGTONPOST();
-});
+// cron.schedule('55 * * * *', () => {
+//     //1h
+// const WASHINGTONPOST = require('./sources/The Washington Post');
+// WASHINGTONPOST();
+// });
 
-cron.schedule('50 * * * *', () => {
-    console.log(18)
-    //1h
-const VICENEWS = require('./sources/Vice News');
-VICENEWS();
-});
+// cron.schedule('50 * * * *', () => {
+//     //1h
+// const VICENEWS = require('./sources/Vice News');
+// VICENEWS();
+// });
 
 
 
