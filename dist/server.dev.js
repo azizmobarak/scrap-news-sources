0f3b15e6430e5062c9fa46bd3e40047806138d6a
aziz mobarak <mobarakaziz9@gmail.com>
2021-03-11 00:53:30 +0100

    dhnet

--- dist/server.dev.js
+++ dist/server.dev.js
@@ -24,12 +24,12 @@ con.connect(process.env.DATABASE, {
   if (err) console.log(err);else {
     console.log('connected');
   }
-}); //cron.schedule('00 */1 * * *', () => {
+});
+cron.schedule('00 */1 * * *', function () {
+  var DHNET = require('./sources/FR/DHNET');
 
-var DHNET = require('./sources/FR/DHNET');
-
-DHNET(); //});
-
+  DHNET();
+});
 app.listen(PORT, function () {
   console.log('connected at ' + PORT);
 });
