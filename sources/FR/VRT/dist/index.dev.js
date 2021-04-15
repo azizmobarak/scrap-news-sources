542639d64824cd956e1a068fd534d9dbffba3d51
aziz mobarak <mobarakaziz9@gmail.com>
2021-04-15 19:49:55 +0000

    update

--- sources/FR/VRT/dist/index.dev.js
+++ sources/FR/VRT/dist/index.dev.js
@@ -11,7 +11,13 @@ var Recaptcha = require('puppeteer-extra-plugin-recaptcha');
 var AdblockerPlugin = require('puppeteer-extra-plugin-adblocker');
 
 var _require = require('../../../function/insertData'),
-    InsertData = _require.InsertData; //block ads
+    InsertData = _require.InsertData;
+
+var _require2 = require('../../../function/formatimage'),
+    FormatImage = _require2.FormatImage;
+
+var _require3 = require('../../../function/sendtoserver'),
+    SendToServer = _require3.SendToServer; //block ads
 
 
 puppeteer.use(AdblockerPlugin()); // stealth
@@ -27,7 +33,7 @@ puppeteer.use(Recaptcha({
 
 }));
 puppeteer.use(puppeteer_agent());
-var Categories = ['politic', 'economy', 'culture'];
+var Categories = ['politique', 'économie', 'culture'];
 
 var VRT = function VRT() {
   (function _callee2() {
@@ -55,7 +61,7 @@ var VRT = function VRT() {
 
           case 9:
             if (!(i < Categories.length)) {
-              _context2.next = 34;
+              _context2.next = 35;
               break;
             }
 
@@ -63,7 +69,7 @@ var VRT = function VRT() {
             Category = Categories[i]; //navigate to category sub route
 
             url = "https://www.vrt.be/vrtnws/fr/rubriques/politique/";
-            if (Category === "economy") url = "https://www.vrt.be/vrtnws/fr/rubriques/economie/";
+            if (Category === "économie") url = "https://www.vrt.be/vrtnws/fr/rubriques/economie/";
             if (Category === "culture") url = "https://www.vrt.be/vrtnws/fr/rubriques/culture---media/";
             _context2.prev = 14;
             _context2.next = 17;
@@ -127,8 +133,8 @@ var VRT = function VRT() {
                     title: titles[j].textContent.trim(),
                     images: typeof images[j] != "undefined" ? images[j].src : null,
                     link: typeof links[j] === "undefined" ? null : links[j].href,
-                    Category: Category,
-                    source: "VRT NEWS",
+                    Category: Category.charAt(0).toUpperCase() + Category.slice(1),
+                    source: "VRT - " + Category.charAt(0).toUpperCase() + Category.slice(1),
                     sourceLink: "https://www.vrt.be",
                     sourceLogo: "https://www.vrt.be/etc.clientlibs/vrtnieuws/clientlibs/clientlib-v2-site/resources/images/og_image.png"
                   });
@@ -140,57 +146,61 @@ var VRT = function VRT() {
 
           case 29:
             PageData = _context2.sent;
-            // console.log(PageData);
-            PageData.map(function (item) {
+            console.log(PageData);
+            PageData.map(function (item, j) {
+              item.images = FormatImage(item.images);
+              setTimeout(function () {
+                SendToServer('fr', item.Category, item.source, item.sourceLogo);
+              }, 2000 * j);
               AllData.push(item);
             });
 
-          case 31:
+          case 32:
             i++;
             _context2.next = 9;
             break;
 
-          case 34:
-            _context2.next = 41;
+          case 35:
+            _context2.next = 42;
             break;
 
-          case 36:
-            _context2.prev = 36;
+          case 37:
+            _context2.prev = 37;
             _context2.t1 = _context2["catch"](7);
             console.log(_context2.t1);
-            _context2.next = 41;
+            _context2.next = 42;
             return regeneratorRuntime.awrap(browser.close());
 
-          case 41:
-            _context2.prev = 41;
-            _context2.next = 44;
+          case 42:
+            _context2.prev = 42;
+            _context2.next = 45;
             return regeneratorRuntime.awrap(GetContent(page, AllData));
 
-          case 44:
-            _context2.next = 50;
+          case 45:
+            _context2.next = 51;
             break;
 
-          case 46:
-            _context2.prev = 46;
-            _context2.t2 = _context2["catch"](41);
-            _context2.next = 50;
+          case 47:
+            _context2.prev = 47;
+            _context2.t2 = _context2["catch"](42);
+            _context2.next = 51;
             return regeneratorRuntime.awrap(browser.close());
 
-          case 50:
-            _context2.next = 52;
+          case 51:
+            _context2.next = 53;
             return regeneratorRuntime.awrap(browser.close());
 
-          case 52:
+          case 53:
           case "end":
             return _context2.stop();
         }
       }
-    }, null, null, [[7, 36], [14, 19], [41, 46]]);
+    }, null, null, [[7, 37], [14, 19], [42, 47]]);
   })();
 };
 
 var GetContent = function GetContent(page, data) {
-  var AllData_WithConetent, i, item, url, Content, author;
+  var AllData_WithConetent, i, item, url, Content, contenthtml, author;
   return regeneratorRuntime.async(function GetContent$(_context3) {
     while (1) {
       switch (_context3.prev = _context3.next) {
@@ -200,18 +210,18 @@ var GetContent = function GetContent(page, data) {
 
         case 2:
           if (!(i < data.length)) {
-            _context3.next = 17;
+            _context3.next = 21;
             break;
           }
 
           item = data[i];
-          url = item.link; // console.log(url)
-
-          _context3.next = 7;
+          url = item.link;
+          console.log(url);
+          _context3.next = 8;
           return regeneratorRuntime.awrap(page["goto"](url));
 
-        case 7:
-          _context3.next = 9;
+        case 8:
+          _context3.next = 10;
           return regeneratorRuntime.awrap(page.evaluate(function () {
             try {
               // first try to get all content
@@ -228,18 +238,29 @@ var GetContent = function GetContent(page, data) {
             }
           }));
 
-        case 9:
+        case 10:
           Content = _context3.sent;
-          _context3.next = 12;
+          _context3.next = 13;
           return regeneratorRuntime.awrap(page.evaluate(function () {
             try {
-              return document.querySelector('.author-info__names').textContent.trim();
+              return document.querySelector('.article__par').innerHTML;
             } catch (_unused3) {
               return null;
             }
           }));
 
-        case 12:
+        case 13:
+          contenthtml = _context3.sent;
+          _context3.next = 16;
+          return regeneratorRuntime.awrap(page.evaluate(function () {
+            try {
+              return document.querySelector('.author-info__names').textContent.trim();
+            } catch (_unused4) {
+              return null;
+            }
+          }));
+
+        case 16:
           author = _context3.sent;
 
           if (Content != null && Content != "") {
@@ -253,20 +274,22 @@ var GetContent = function GetContent(page, data) {
               sourceLink: item.sourceLink,
               sourceLogo: item.sourceLogo,
               author: author,
-              content: Content
+              content: Content,
+              contenthtml: contenthtml
             });
           }
 
-        case 14:
+        case 18:
           i++;
           _context3.next = 2;
           break;
 
-        case 17:
-          _context3.next = 19;
+        case 21:
+          console.log(AllData_WithConetent);
+          _context3.next = 24;
           return regeneratorRuntime.awrap(InsertData(AllData_WithConetent));
 
-        case 19:
+        case 24:
         case "end":
           return _context3.stop();
       }
--- sources/FR/VRT/index.js
+++ sources/FR/VRT/index.js
@@ -1,11 +1,13 @@
 'use strict';
 
-const puppeteer  = require('puppeteer-extra');
+const puppeteer = require('puppeteer-extra');
 const puppeteer_stealth = require('puppeteer-extra-plugin-stealth');
 const puppeteer_agent = require('puppeteer-extra-plugin-anonymize-ua');
 const Recaptcha = require('puppeteer-extra-plugin-recaptcha');
 const AdblockerPlugin = require('puppeteer-extra-plugin-adblocker')
-const {InsertData} = require('../../../function/insertData');
+const { InsertData } = require('../../../function/insertData');
+const { FormatImage } = require('../../../function/formatimage');
+const { SendToServer } = require('../../../function/sendtoserver');
 
 //block ads
 puppeteer.use(AdblockerPlugin());
@@ -21,169 +23,184 @@ puppeteer.use(
 
 puppeteer.use(puppeteer_agent());
 
-var Categories=['politic','economy','culture'];
+var Categories = ['politique', 'économie', 'culture'];
 
-const VRT = () =>{
-    (async()=>{
-       var browser =await puppeteer.launch({
-        headless: true,
-        args: [
-            '--enable-features=NetworkService',
-            '--no-sandbox',
-            '--disable-setuid-sandbox',
-            '--disable-dev-shm-usage',
-            '--shm-size=3gb',
-        ],
-       });
-
-
-       var page = await browser.newPage(); 
-
- 
-var AllData=[]; 
-
-try{
-// boucle on categories started 
-for(let i=0;i<Categories.length;i++){
-
-    //get the right category by number
-    var Category = Categories[i]
-    //navigate to category sub route
-    var url ="https://www.vrt.be/vrtnws/fr/rubriques/politique/";
-
-    if(Category==="economy") url="https://www.vrt.be/vrtnws/fr/rubriques/economie/";
-    if(Category==="culture") url="https://www.vrt.be/vrtnws/fr/rubriques/culture---media/";
-
-    try{
-        await page.goto(url);
-       // await page.click('.iubenda-cs-accept-btn')
-       }catch{
-        await page.goto(url);
-       }
-      //  await page.waitForNavigation({ waitUntil: 'networkidle0' }) //networkidle0
+const VRT = () => {
+    (async () => {
+        var browser = await puppeteer.launch({
+            headless: true,
+            args: [
+                '--enable-features=NetworkService',
+                '--no-sandbox',
+                '--disable-setuid-sandbox',
+                '--disable-dev-shm-usage',
+                '--shm-size=3gb',
+            ],
+        });
 
 
-      await page.evaluate(()=>{
+        var page = await browser.newPage();
 
-        var totalHeight = 0;
-            var distance = 100;
-            var timer = setInterval(async() => {
-                var scrollHeight = document.body.scrollHeight;
-               window.scrollBy(0, distance);
-                totalHeight += distance;
 
-                if(totalHeight >= 2000){
-                    clearInterval(timer);
-                    resolve();
+        var AllData = [];
+
+        try {
+            // boucle on categories started 
+            for (let i = 0; i < Categories.length; i++) {
+
+                //get the right category by number
+                var Category = Categories[i]
+                //navigate to category sub route
+                var url = "https://www.vrt.be/vrtnws/fr/rubriques/politique/";
+
+                if (Category === "économie") url = "https://www.vrt.be/vrtnws/fr/rubriques/economie/";
+                if (Category === "culture") url = "https://www.vrt.be/vrtnws/fr/rubriques/culture---media/";
+
+                try {
+                    await page.goto(url);
+                    // await page.click('.iubenda-cs-accept-btn')
+                } catch {
+                    await page.goto(url);
                 }
-            }, 100);
-    });
+                //  await page.waitForNavigation({ waitUntil: 'networkidle0' }) //networkidle0
 
-     await page.waitFor(2000)
 
-    
-         // get the data from the page
-var PageData = await page.evaluate((Category)=>{
-               
-    var images = document.querySelectorAll('article img');
-    var links = document.querySelectorAll('.vrt-teaser-wrapper a');
-    var titles = document.querySelectorAll('article h2');
-       
-         
-        var data =[];
-         for(let j=0;j<5;j++){
-              if(typeof(titles[j])!="undefined" && typeof(links[j])!="undefined"){
-                   data.push({
-                       time : Date.now(),
-                       title : titles[j].textContent.trim(),
-                       images : typeof(images[j])!="undefined" ? images[j].src : null,
-                       link : typeof(links[j])==="undefined" ? null : links[j].href ,
-                       Category:Category,
-                       source :"VRT NEWS",
-                       sourceLink:"https://www.vrt.be",
-                       sourceLogo:"https://www.vrt.be/etc.clientlibs/vrtnieuws/clientlibs/clientlib-v2-site/resources/images/og_image.png"
-                      });
-                   }
-               }
-                      return data;
-     },Category);
-          // console.log(PageData);
-            PageData.map(item=>{
-            AllData.push(item)
-                    });
-       }}catch(e){
-        console.log(e)
+                await page.evaluate(() => {
+
+                    var totalHeight = 0;
+                    var distance = 100;
+                    var timer = setInterval(async () => {
+                        var scrollHeight = document.body.scrollHeight;
+                        window.scrollBy(0, distance);
+                        totalHeight += distance;
+
+                        if (totalHeight >= 2000) {
+                            clearInterval(timer);
+                            resolve();
+                        }
+                    }, 100);
+                });
+
+                await page.waitFor(2000)
+
+
+                // get the data from the page
+                var PageData = await page.evaluate((Category) => {
+
+                    var images = document.querySelectorAll('article img');
+                    var links = document.querySelectorAll('.vrt-teaser-wrapper a');
+                    var titles = document.querySelectorAll('article h2');
+
+
+                    var data = [];
+                    for (let j = 0; j < 5; j++) {
+                        if (typeof (titles[j]) != "undefined" && typeof (links[j]) != "undefined") {
+                            data.push({
+                                time: Date.now(),
+                                title: titles[j].textContent.trim(),
+                                images: typeof (images[j]) != "undefined" ? images[j].src : null,
+                                link: typeof (links[j]) === "undefined" ? null : links[j].href,
+                                Category: Category.charAt(0).toUpperCase() + Category.slice(1),
+                                source: "VRT - " + Category.charAt(0).toUpperCase() + Category.slice(1),
+                                sourceLink: "https://www.vrt.be",
+                                sourceLogo: "https://www.vrt.be/etc.clientlibs/vrtnieuws/clientlibs/clientlib-v2-site/resources/images/og_image.png"
+                            });
+                        }
+                    }
+                    return data;
+                }, Category);
+                console.log(PageData);
+
+                PageData.map((item, j) => {
+                    item.images = FormatImage(item.images);
+                    setTimeout(() => {
+                        SendToServer('fr', item.Category, item.source, item.sourceLogo)
+                    }, 2000 * j);
+                    AllData.push(item)
+                });
+            }
+        } catch (e) {
+            console.log(e)
+            await browser.close();
+        }
+
+        try {
+            await GetContent(page, AllData);
+        } catch (e) {
+            // console.log(e);
+            await browser.close();
+        }
+
         await browser.close();
-       }
-
-       try{
-       await GetContent(page,AllData);
-       }catch(e){
-        // console.log(e);
-        await browser.close();
-       }
-
-     await browser.close();
     })();
 }
 
 
 
-const GetContent = async(page,data)=>{
-      
-    var AllData_WithConetent=[];
-    
-    for(var i=0;i<data.length;i++){
-    
+const GetContent = async (page, data) => {
+
+    var AllData_WithConetent = [];
+
+    for (var i = 0; i < data.length; i++) {
+
         var item = data[i];
         var url = item.link;
 
-       // console.log(url)
+        console.log(url)
         await page.goto(url);
-    
-        var Content = await page.evaluate(()=>{
-        
-            try{
-            // first try to get all content
-             var second_text = document.querySelectorAll('.article__par p');
-             var scond_content ="";
-             for(let i=0;i<second_text.length;i++){
-                scond_content = scond_content +"\n"+second_text[i].textContent;
-             }
-              return scond_content;
-            }catch{
-               return null;
+
+        var Content = await page.evaluate(() => {
+
+            try {
+                // first try to get all content
+                var second_text = document.querySelectorAll('.article__par p');
+                var scond_content = "";
+                for (let i = 0; i < second_text.length; i++) {
+                    scond_content = scond_content + "\n" + second_text[i].textContent;
+                }
+                return scond_content;
+            } catch {
+                return null;
             }
         });
 
-     var author = await page.evaluate(()=>{
-         try{
-        return document.querySelector('.author-info__names').textContent.trim();
-         }catch{
-        return null;
-         }
-     })
+        var contenthtml = await page.evaluate(() => {
+            try {
+                return document.querySelector('.article__par').innerHTML;
+            } catch {
+                return null;
+            }
+        });
 
-    
-    if(Content!=null && Content!=""){
-          AllData_WithConetent.push({
-                time : Date.now(),
-                title : item.title,
-                link : item.link,
-                images : item.images,
-                Category:item.Category,
-                source :item.source,
-                sourceLink:item.sourceLink,
-                sourceLogo:item.sourceLogo,
-                author : author,
-                content:Content
-          });
-       }
-    
+        var author = await page.evaluate(() => {
+            try {
+                return document.querySelector('.author-info__names').textContent.trim();
+            } catch {
+                return null;
+            }
+        })
+
+
+        if (Content != null && Content != "") {
+            AllData_WithConetent.push({
+                time: Date.now(),
+                title: item.title,
+                link: item.link,
+                images: item.images,
+                Category: item.Category,
+                source: item.source,
+                sourceLink: item.sourceLink,
+                sourceLogo: item.sourceLogo,
+                author: author,
+                content: Content,
+                contenthtml: contenthtml
+            });
+        }
+
     }
- // console.log(AllData_WithConetent)
-  await InsertData(AllData_WithConetent);
+    console.log(AllData_WithConetent)
+    await InsertData(AllData_WithConetent);
 }
 
 
-module.exports=VRT;
+module.exports = VRT;
